b0b4b0141ffaf6cc1c40ac6d0140f794839d3a28
Artista99 <159783294+Artista99@users.noreply.github.com>
2026-05-23 14:32:17 +0200

    new commit

--- app.py
+++ app.py
@@ -2,23 +2,14 @@ import streamlit as st
 
 ALLOWED_EMAILS = st.secrets.auth.allowed_emails
 
-def login_screen():
-    st.title("Private app")
-    st.write("Please log in with Google.")
-    st.button("Log in", on_click=st.login)
-
 if not st.user.is_logged_in:
-    login_screen()
+    st.button("Log in with Google", on_click=st.login)
     st.stop()
 
 if st.user.email not in ALLOWED_EMAILS:
-    st.error("You are not authorized to access this app.")
-    st.write(f"Logged in as: {st.user.email}")
+    st.error("Not authorized")
     st.button("Log out", on_click=st.logout)
     st.stop()
 
 st.title("Hello World")
-st.write(f"Welcome, {st.user.name}!")
-st.write("this will be my personal dashbaord")
-st.button("Log out", on_click=st.logout)
-
+st.write(f"Welcome {st.user.email}")
